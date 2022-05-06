b54d0b86b5dc97f2a596ec3395e6b6b899898579
ignac8 <995943+ignac8@users.noreply.github.com>
2022-05-06 16:56:03 +0200

    Fixed quoting

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ if __name__ == '__main__':
         with open(f'{templates_dir}/{excel_dir}/{filename}', mode='r', newline='') as template_file:
             os.makedirs(f'{results_dir}/{excel_dir}', exist_ok=True)
             with open(f'{results_dir}/{excel_dir}/{filename}', mode='w', newline='') as result_file:
-                reader = csv.DictReader(template_file, delimiter='\t')
-                writer = csv.DictWriter(result_file, delimiter='\t', fieldnames=reader.fieldnames)
+                reader = csv.DictReader(template_file, delimiter='\t', dialect="excel-tab", quoting=csv.QUOTE_NONE,
+                                        quotechar=None)
+                writer = csv.DictWriter(result_file, delimiter='\t', fieldnames=reader.fieldnames,
+                                        dialect="excel-tab", quoting=csv.QUOTE_NONE, quotechar=None)
                 writer.writeheader()
                 for row in reader:
                     writer.writerow(row)
