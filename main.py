c3a175d77a795bbe123e1a7a7f18fc7c2bd389c4
ignac8 <995943+ignac8@users.noreply.github.com>
2022-05-06 16:43:21 +0200

    Fixed whiteline handling and header

--- main.py
+++ main.py
@@ -6,10 +6,11 @@ if __name__ == '__main__':
     templates_dir = 'templates'
     results_dir = 'results'
     for filename in os.listdir(f'{templates_dir}/{excel_dir}'):
-        with open(f'{templates_dir}/{excel_dir}/{filename}', mode='r') as template_file:
+        with open(f'{templates_dir}/{excel_dir}/{filename}', mode='r', newline='') as template_file:
             os.makedirs(f'{results_dir}/{excel_dir}', exist_ok=True)
-            with open(f'{results_dir}/{excel_dir}/{filename}', mode='w') as result_file:
+            with open(f'{results_dir}/{excel_dir}/{filename}', mode='w', newline='') as result_file:
                 reader = csv.DictReader(template_file, delimiter='\t')
                 writer = csv.DictWriter(result_file, delimiter='\t', fieldnames=reader.fieldnames)
+                writer.writeheader()
                 for row in reader:
                     writer.writerow(row)
