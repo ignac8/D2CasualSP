11c46d9156c23eded09a34ac52231e8bb5bd4e18
ignac8 <ignac8@op.pl>
2022-05-06 17:46:25 +0200

    Added more common drops

--- modifiers.py
+++ modifiers.py
@@ -2,8 +2,29 @@ import csv
 import os
 
 
-def no_drop(filename, row):
+def disable_no_drop(filename, row):
     if filename == 'treasureclassex.txt':
         if row['NoDrop']:
             row['NoDrop'] = 0
     return row
+
+
+def more_common_drops(filename, row):
+    if filename == 'itemratio.txt':
+        for value in row.items():
+            if value[1].isnumeric() and int(value[1]) and not (value[0].endswith("Divisor")) and not (
+            value[0].endswith("Min")):
+                row[value[0]] = int(int(value[1]) / 10)
+    return row
+
+
+def better_rune_drop(filename, row):
+    pass
+
+
+def disable_ladder_only(filename, row):
+    pass
+
+
+def more_monsters(filename, row):
+    pass
