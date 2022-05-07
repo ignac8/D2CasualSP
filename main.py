a06b2075fb904ae9396df8c41a75d85902a4446a
ignac8 <ignac8@op.pl>
2022-05-07 16:19:50 +0200

    Rework path handling

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import csv
 import os
+import shutil
+from pathlib import Path
 
 import modifiers
 
@@ -7,13 +9,17 @@ import modifiers
 def main():
     modifiers_list = list(filter(lambda x: callable(x) and not (x.__name__.startswith('__')),
                                  map(lambda x: getattr(modifiers, x), dir(modifiers))))
-    excel_dir = 'data/data/global/excel'
-    templates_dir = 'templates'
-    results_dir = 'results'
-    for filename in os.listdir(f'{templates_dir}/{excel_dir}'):
-        with open(f'{templates_dir}/{excel_dir}/{filename}', mode='r', newline='') as template_file:
-            os.makedirs(f'{results_dir}/{excel_dir}', exist_ok=True)
-            with open(f'{results_dir}/{excel_dir}/{filename}', mode='w', newline='') as result_file:
+    templates = Path('templates')
+    results = Path('results')
+    excel = Path('D2RCasualSP').joinpath(Path('D2RCasualSP.mpq')).joinpath(Path('data')).joinpath(
+        Path('global')).joinpath(Path('excel'))
+
+    shutil.rmtree(results, ignore_errors=True)
+    shutil.copytree(templates, results)
+
+    for file in templates.joinpath(excel).iterdir():
+        with open(file, mode='r', newline='') as template_file:
+            with open(results.joinpath(excel).joinpath(file.name), mode='w', newline='') as result_file:
                 reader = csv.DictReader(template_file, delimiter='\t', dialect='excel-tab', quoting=csv.QUOTE_NONE,
                                         quotechar=None)
                 writer = csv.DictWriter(result_file, delimiter='\t', fieldnames=reader.fieldnames,
@@ -21,7 +27,7 @@ def main():
                 writer.writeheader()
                 for row in reader:
                     for modifier in modifiers_list:
-                        row = modifier(filename, row)
+                        row = modifier(file, row)
                     writer.writerow(row)
 
 
